2091dfce85cf5ab3e1c8e84d2bff958895a43fe9
ferran <ferran@ferran-GE62VR-6RF>
2018-11-25 12:27:30 +0100

    Elimina paquet enumerations

---
+++ Festival_Projecte/src/Classes/Espectador.java
@@ -0,0 +1,17 @@
+package Classes;
+
+public class Espectador  extends Persona{
+	
+	private String tipusEntrada;
+	private int diesAssistencia;
+	private float pressupost;
+	
+	private Espectador(String nom, String cognoms, String dNI, int edat) {
+		super(nom, cognoms, dNI, edat);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+}
