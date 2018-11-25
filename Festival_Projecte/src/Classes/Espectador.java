c51d3b450dcec32d7af1276be0c04855678256fa
ferran <ferran@ferran-GE62VR-6RF>
2018-11-25 12:40:09 +0100

    Classe Espectador versió 2

--- Festival_Projecte/src/Classes/Espectador.java
+++ Festival_Projecte/src/Classes/Espectador.java
@@ -12,8 +12,41 @@ public class Espectador  extends Persona{
 		this.diesAssistencia = diesAssistencia;
 		this.pressupost = pressupost;
 	}
+	public String getTipusEntrada() {
+		return tipusEntrada;
+	}
+	public void setTipusEntrada(String tipusEntrada) {
+		this.tipusEntrada = tipusEntrada;
+	}
+	public int getDiesAssistencia() {
+		return diesAssistencia;
+	}
+	public void setDiesAssistencia(int diesAssistencia) {
+		this.diesAssistencia = diesAssistencia;
+	}
+	public float getPressupost() {
+		return pressupost;
+	}
+	public void setPressupost(float pressupost) {
+		this.pressupost = pressupost;
+	}
+	@Override
+	public void AccedeixenAlRecinte() {
+		// TODO Auto-generated method stub
+		super.AccedeixenAlRecinte();
+	}
+	@Override
+	public void SurtenDelRecinte() {
+		// TODO Auto-generated method stub
+		super.SurtenDelRecinte();
+	}
 	
-	
+	public float Pagar(float preu) {
+		
+		return pressupost - preu;
+		
+		
+	}
 	
 	
 	
